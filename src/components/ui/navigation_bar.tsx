a41e044a1ca1dd80847ac120f9360f2b5a4abe54
beermerchant <up2283950@myport.ac.uk>
2026-07-31 11:14:51 -0500

    added a new page to the website

--- src/components/ui/navigation_bar.tsx
+++ src/components/ui/navigation_bar.tsx
@@ -6,20 +6,25 @@ import { ModeToggle } from "./theme-toggle";
 
 export default function NavigationBar() {
     return (
-    <div>
-        <Card className="flex flex-row justify-start h-min-12rem w-screen p-5 rounded-none">
-            <a
-                href="/home"
-                className={buttonVariants({ variant: "default" })}
-            >
-                Home
-            </a>
-            <Button>about</Button>
-            <Button>fun stuff</Button>
-            <div className="flex flex-row justify-end w-full">
-            <ModeToggle />
-            </div>
-        </Card>
-    </div>
+        <div>
+            <Card className="flex flex-row justify-start h-min-12rem w-screen p-5 rounded-none">
+                <a
+                    href="/home"
+                    className={buttonVariants({ variant: "default" })}
+                >
+                    ai_notice
+                </a>
+                <a
+                    href="/ai_usage"
+                    className={buttonVariants({ variant: "default" })}
+                >
+                    home
+                </a>
+                <Button>fun stuff</Button>
+                <div className="flex flex-row justify-end w-full">
+                    <ModeToggle />
+                </div>
+            </Card>
+        </div>
     )
 }
